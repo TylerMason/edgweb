8d62457c8207365efbe5c9ffe9dae36a36a6970f
TylerMason <tymasn@gmail.com>
2024-07-08 16:59:20 -0700

    update to links

--- app/page.jsx
+++ app/page.jsx
@@ -34,14 +34,23 @@ export default function HomePage() {
   return (
     <main className="flex flex-col min-h-screen items-center justify-between bg-slate-200">
       <div className="flex flex-col lg:flex-row w-full h-screen">
-        <div className="lg:w-[300px] w-full h-full p-4 order-2 lg:order-none flex flex-col">
+        <div className="lg:w-[300px] w-full h-full p-4 order-2 lg:order-none flex flex-col justify-between">
           <div className='space-y-8'>
             <div className="pt-4">
               <Image src="/logo.png" alt="Logo" width={300} height={100} objectFit="contain" />
             </div>
             <Categories selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+
+            {/* Dummy hyperlinks */}
+            <div className="pt-4 text-center">
+              <a href="https://drive.google.com/file/d/16T5daZztGiJ5KY8IF6v-hIlL2OhWfyCx/view?usp=sharing" className="text-blue-500 hover:underline block" target="_blank" rel="noopener noreferrer">Builder Repair Locations</a>
+              <a href="https://drive.google.com/file/d/1Qrq3-n2GuBBgmjRz5yEtz3IKa2SNVCXA/view?usp=sharing" className="text-blue-500 hover:underline block mt-2" target="_blank" rel="noopener noreferrer">EDG DT Locations</a>
+              <a href="https://drive.google.com/file/d/1Hi_XQKN8wSifNxnUDyMB6DS32pxiPgwt/view?usp=sharing" className="text-blue-500 hover:underline block mt-2" target="_blank" rel="noopener noreferrer">Units Inspected</a>
+            </div>
           </div>
+
           <div className="flex justify-center items-end">
+            {/* Sign Out button */}
             <button
               onClick={() => signOut({ callbackUrl: '/login' })}
               className="w-auto mt-4 p-2 bg-red-500 text-white rounded"
@@ -50,6 +59,7 @@ export default function HomePage() {
             </button>
           </div>
         </div>
+
         <div className="flex-grow flex items-center justify-center order-1 lg:order-none bg-white">
           <PanZoomSVG selectedCategory={selectedCategory} />
         </div>
